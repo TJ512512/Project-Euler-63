dccf0853c2969a4e9d6b650650935778cbf5d459
TJ512512 <160138699+TJ512512@users.noreply.github.com>
2025-01-05 13:34:22 +0000

    Add files via upload
    This is the code for project Euler challenge 63

---
+++ Project Euler 63/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Net.Http.Headers;
+using System.Data.SqlTypes;
+namespace Project_Euler_63
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger count = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                for (int j = 1; j <= 100; j++)
+                {
+                    BigInteger number = BigInteger.Pow(i, j);
+                    if (CheckingTheNumbers(number, j) == true)
+                    {
+                        Console.WriteLine(number + " = " + i + "^" + j);
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+        static bool CheckingTheNumbers(BigInteger number, BigInteger power)
+        {
+            BigInteger NumberLength = number.ToString().Length;
+            if (NumberLength == power)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
